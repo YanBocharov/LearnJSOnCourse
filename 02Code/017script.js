996357741275a85d34d5c0b649225c0f99e53f8f
Yan <yan.bocharovv@gmail.com>
2022-12-03 14:39:43 +0300

    first

---
+++ 02Code/017script.js
@@ -0,0 +1,22 @@
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+let result = '';
+const length = 11;
+
+for (let i = 1; i < length; i++) {
+
+    for (let j = 0; j < i; j++) {
+        result += '*';
+    };
+
+    result += '\n';
+};
+
+console.log(result); 
+
+ 
